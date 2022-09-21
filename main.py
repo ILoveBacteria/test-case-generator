f462a698c07aed88734baf51e985028ab77e2d40
Moein Arabi <moein.mo81@gmail.com>
2022-09-21 11:14:30 +0430

    Refactor code

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ inputLines = int(input('number of input lines: '))
 outputLines = int(input('number of output lines: '))
 resumeFrom = int(input('resume number from? '))
 
+os.makedirs('test/in', exist_ok=True)
+os.makedirs('test/out', exist_ok=True)
+
 for i in range(resumeFrom, 100):
     inputTest = []
     outputTest = []
@@ -17,8 +20,6 @@ for i in range(resumeFrom, 100):
 
     print('Generating...')
 
-    os.makedirs('test/in', exist_ok=True)
-    os.makedirs('test/out', exist_ok=True)
     with open(f'test/in/input{i}.txt', 'w') as file:
         file.write('\n'.join(inputTest))
     with open(f'test/out/output{i}.txt', 'w') as file:
