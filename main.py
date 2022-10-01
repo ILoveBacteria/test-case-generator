956a15c133cdbeec9b527806851fd14b2ecfefb3
Moein Arabi <moein.mo81@gmail.com>
2022-10-01 13:00:26 +0330

    Variable input and output lines

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 
-inputLines = int(input('number of input lines: '))
-outputLines = int(input('number of output lines: '))
+inputLines = int(input('number of input lines(0 for var): '))
+outputLines = int(input('number of output lines(0 for var): '))
 resumeFrom = int(input('resume number from? '))
+
+variable_lines = False
+if inputLines == 0 or outputLines == 0:
+    variable_lines = True
 
 os.makedirs('test/in', exist_ok=True)
 os.makedirs('test/out', exist_ok=True)
@@ -12,6 +16,10 @@ for i in range(resumeFrom, 100):
     outputTest = []
 
     print(f'Test case number {i}')
+
+    if variable_lines:
+        inputLines = int(input('number of input lines: '))
+        outputLines = int(input('number of output lines: '))
 
     for _ in range(inputLines):
         inputTest.append(input('in: '))
